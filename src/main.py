5cefabeb9c29d2df2d3135db7c67a3260aa011a7
Vincent112704 <112465584+Vincent112704@users.noreply.github.com>
2026-04-21 21:04:43 +0800

    feat(main.py): done setting up telegram webhook & configuring endpoint in telegram

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from typing import Optional, List
+import logging
 
+logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
 
@@ -9,21 +12,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    message = "Debugger test running"
-    print(message)
-    return {"message": message}
-
-
-@app.get("/debug")
-def debug_test():
-    number = 10
-    result = number * 2   # ← set breakpoint here
-    return {"result": result}
-
-
-@app.get("/sum/{a}/{b}")
-def sum_numbers(a: int, b: int):
-    total = a + b   # ← breakpoint here
-    return {"a": a, "b": b, "sum": total}
+@app.post("/webhook")
+async def telegram_webhook(request: Request):
+    '''
+    args: Receives user message from telegram
+    returns: status 200 if successful
+    '''
+    try: 
+        #todo
+        #process the user message in the background
+        user_message = await request.json()
+        print(user_message)
+        return JSONResponse(content={"status": "success"}, status_code=200)
+    except Exception as e:
+        logging.error(f"Error processing Telegram webhook: {e}")
+        return JSONResponse(content={"status": "error", "message": str(e)}, status_code=500)
